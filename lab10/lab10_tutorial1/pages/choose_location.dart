00d52371c06e33fe006a8eaa96b845c8c5cd6777
Darshit Bhuva <85541602+DarshitBhuva@users.noreply.github.com>
2022-09-18 10:27:14 +0530

    Add files via upload

---
+++ lab10/lab10_tutorial1/pages/choose_location.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/material.dart';
+
+
+class ChooseLocation extends StatefulWidget {
+
+// const ChooseLocation({Key? key}) : super(key: key);
+
+  @override
+
+  State<ChooseLocation> createState() => _ChooseLocationState();
+
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    print('Build Function Run in Choose Location...');
+    return Scaffold(
+
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("CHOOSE LOCATION"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+
+      // body: ElevatedButton(
+      //   onPressed: (){
+      //     setState((){
+      //       counter += 1;
+      //     });
+      //   },
+      //   child: Text('Counter is: $counter'),
+      // ),
+
+    );
+  }
+}
